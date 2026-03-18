268cc5d405ebb0de02d86c28cff688f37b771a25
9893 <9893@INF3-PC08>
2026-03-18 14:00:11 +0100

    .

--- Action_e_Func/Program.cs
+++ Action_e_Func/Program.cs
@@ -7,15 +7,17 @@ namespace Action_e_Func
     public class Esercizio3
     {
         // 1. Metodo che combina Func (per filtrare) e Action (per eseguire l'output)
-        public static void ElaboraArray(int[] numeri, Func<int, bool> filtro, Action<int> stampa)
+        public static void FiltraArray(int[] numeri, Func<int, bool> filtro, int[] numeriFiftrati)
         {
+            int i = 0;
             foreach (int numero in numeri)
             {
                 // Se il numero passa il filtro (il Func restituisce true)...
                 if (filtro(numero))
                 {
                     // ...allora esegui l'azione di stampa (l'Action)
-                    stampa(numero);
+                    numeriFiftrati[i] = numero;
+                    i++;
                 }
             }
         }
@@ -27,22 +29,41 @@ namespace Action_e_Func
 
             // Creazione array da 1 a 20 usando LINQ per comodità
             int[] dati = Enumerable.Range(1, 20).ToArray();
+            int[] numerifiltrati = new int[dati.Length];
 
             // Prima esecuzione: filtro i pari, stampo il numero puro
             Console.WriteLine("Numeri pari:");
-            ElaboraArray(
+            FiltraArray(
                 dati,
                 n => n % 2 == 0,
-                n => Console.WriteLine(n)
-            );
+numerifiltrati
+                );
+            Console.WriteLine("array originale ");
+            foreach (var num in dati)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("array modificato");
+
+            foreach (var num in numerifiltrati)
+            {
+                Console.WriteLine(num);
+            }
 
             // Seconda esecuzione: filtro i > 10, stampo con string interpolation
             Console.WriteLine("\nNumeri maggiori di 10:");
-            ElaboraArray(
+            FiltraArray(
                 dati,
                 n => n > 10,
-                n => Console.WriteLine($"Numero trovato: {n}")
-            );
+numerifiltrati
+                );
+
+            foreach (var num in numerifiltrati)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 
